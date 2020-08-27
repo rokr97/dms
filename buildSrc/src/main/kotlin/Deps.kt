2ba296117e0a081fce581a9543364c2efd37afe3
rokr <rodion.krivoshein@gmail.com>
2020-08-27 16:26:16 +0300

    Extract application launcher into separate module

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,6 +1,5 @@
 object Deps {
     val mapstruct = "1.3.1.Final"
-    val spring_boot = "2.3.3.RELEASE"
     val hibernate_validator = "6.1.5.Final"
     val spring_doc_open_api = "1.4.4"
     val javax_validation_api = "2.0.1.Final"
